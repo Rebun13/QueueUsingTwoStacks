b800e5418d52114ead6252043a7e4a47d58bbe56
Rebun <rubengdopazo@gmail.com>
2024-02-06 20:04:53 +0000

    All tests passed

--- QueueUsingTwoStacks.cpp
+++ QueueUsingTwoStacks.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <vector>
 #include <string>
 #include <stack>
+#include <chrono>
+#include <cstdio>
 
 using namespace std;
 
@@ -11,9 +14,49 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 /*
-* The function accepts string query and stack<int> stack as parameters.
+ * The function accepts string query and two stack<int> stack as parameters.
  */
-void queueTwoStacks(string query, stack<int> &s) {
+void queueTwoStacks(string query, stack<int> &s, stack<int> &r)
+{
+    switch (query[0])
+    {
+    case '1': // push
+        s.push(stoi(query.substr(2, query.size() - 2)));
+        break;
+    case '2': // pop
+        if (!r.empty())
+        {
+            r.pop();
+        }
+        else
+        {
+            while (s.size() > 1)
+            {
+                r.push(s.top());
+                s.pop();
+            }
+            s.pop();
+            break;
+        }
+        break;
+    case '3': // top
+        if (r.empty())
+        {
+            while (!s.empty())
+            {
+                r.push(s.top());
+                s.pop();
+            }
+        }
+        cout << r.top() << endl;
+        break;
+    default:
+        break;
+    }
+}
+
+void queueTwoStacksAlt(string query, stack<int> s)
+{
     stack<int> copiedStack = s;
     stack<int> reverseStack;
     switch (query[0])
@@ -22,55 +65,24 @@ void queueTwoStacks(string query, stack<int> &s) {
         s.push(stoi(query.substr(2, query.size() - 2)));
         break;
     case '2': // pop
-        while(s.size() > 1) {
+        while (s.size() > 1)
+        {
             reverseStack.push(s.top());
             s.pop();
         }
         s.pop();
-        while(!reverseStack.empty()) {
+        while (!reverseStack.empty())
+        {
             s.push(reverseStack.top());
             reverseStack.pop();
         }
         break;
     case '3': // top
-        while(copiedStack.size() > 1) {
+        while (copiedStack.size() > 1)
+        {
             copiedStack.pop();
         }
         cout << copiedStack.top() << endl;
-        break;
-    default:
-        break;
-    }
-}
-
-void queueTwoStacksAlt(string query, stack<int> s) {
-    stack<int> reverseStack;
-    switch (query[0])
-    {
-    case '1':
-        s.push(stoi(query.substr(2, query.size() - 2)));
-        break;
-    case '2':
-        while(s.size() > 1) {
-            reverseStack.push(s.top());
-            s.pop();
-        }
-        s.pop();
-        while(!reverseStack.empty()) {
-            s.push(reverseStack.top());
-            reverseStack.pop();
-        }
-        break;
-    case '3':
-        while(s.size() > 1) {
-            reverseStack.push(s.top());
-            s.pop();
-        }
-        cout << s.top() << endl;
-        while(!reverseStack.empty()) {
-            s.push(reverseStack.top());
-            reverseStack.pop();
-        }
         break;
     default:
         break;
@@ -81,49 +93,51 @@ int main()
 {
     string t_temp;
     getline(cin, t_temp);
-
     int t = stoi(ltrim(rtrim(t_temp)));
-    stack<int> s;
-
-    for (int t_itr = 0; t_itr < t; t_itr++) {
+    stack<int> s, r;
+    for (int t_itr = 0; t_itr < t; t_itr++)
+    {
         string m_temp;
         getline(cin, m_temp);
-        
-        queueTwoStacks(ltrim(rtrim(m_temp)), s);
+        queueTwoStacks(ltrim(rtrim(m_temp)), s, r);
     }
-
     return 0;
 }
 
-string ltrim(const string &str) {
+string ltrim(const string &str)
+{
     string s(str);
 
     s.erase(
         s.begin(),
-        find_if(s.begin(), s.end(), [](unsigned char c) { return !ispunct(c); })
-    );
+        find_if(s.begin(), s.end(), [](unsigned char c)
+                { return !ispunct(c); }));
 
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
 
     s.erase(
-        find_if(s.rbegin(), s.rend(), [](unsigned char c) { return !ispunct(c); }).base(),
-        s.end()
-    );
+        find_if(s.rbegin(), s.rend(), [](unsigned char c)
+                { return !ispunct(c); })
+            .base(),
+        s.end());
 
     return s;
 }
 
-vector<string> split(const string &str) {
+vector<string> split(const string &str)
+{
     vector<string> tokens;
 
     string::size_type start = 0;
     string::size_type end = 0;
 
-    while ((end = str.find(" ", start)) != string::npos) {
+    while ((end = str.find(" ", start)) != string::npos)
+    {
         tokens.push_back(str.substr(start, end - start));
 
         start = end + 1;
@@ -133,4 +147,3 @@ vector<string> split(const string &str) {
 
     return tokens;
 }
-
